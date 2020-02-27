c3172a383b8fade3f61fbc7adde994baa8851971
Nick <npk001@mail.ru>
2020-02-27 23:09:06 +0300

    My first rep

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CurList1 = new System.Windows.Forms.ComboBox();
+            this.CurList2 = new System.Windows.Forms.ComboBox();
+            this.Value1 = new System.Windows.Forms.TextBox();
+            this.Accept = new System.Windows.Forms.Button();
+            this.Value2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CurList1
+            // 
+            this.CurList1.FormattingEnabled = true;
+            this.CurList1.Location = new System.Drawing.Point(12, 12);
+            this.CurList1.Name = "CurList1";
+            this.CurList1.Size = new System.Drawing.Size(183, 21);
+            this.CurList1.TabIndex = 0;
+            this.CurList1.Text = "Австралийский доллар";
+            // 
+            // CurList2
+            // 
+            this.CurList2.FormattingEnabled = true;
+            this.CurList2.Location = new System.Drawing.Point(237, 12);
+            this.CurList2.Name = "CurList2";
+            this.CurList2.Size = new System.Drawing.Size(183, 21);
+            this.CurList2.TabIndex = 0;
+            this.CurList2.Text = "Австралийский доллар";
+            // 
+            // Value1
+            // 
+            this.Value1.Location = new System.Drawing.Point(12, 39);
+            this.Value1.Name = "Value1";
+            this.Value1.Size = new System.Drawing.Size(183, 20);
+            this.Value1.TabIndex = 1;
+            this.Value1.Text = "0";
+            // 
+            // Accept
+            // 
+            this.Accept.Location = new System.Drawing.Point(180, 76);
+            this.Accept.Name = "Accept";
+            this.Accept.Size = new System.Drawing.Size(75, 23);
+            this.Accept.TabIndex = 2;
+            this.Accept.Text = "Рассчитать";
+            this.Accept.UseVisualStyleBackColor = true;
+            this.Accept.Click += new System.EventHandler(this.Accept_Click);
+            // 
+            // Value2
+            // 
+            this.Value2.AutoSize = true;
+            this.Value2.Location = new System.Drawing.Point(234, 46);
+            this.Value2.Name = "Value2";
+            this.Value2.Size = new System.Drawing.Size(13, 13);
+            this.Value2.TabIndex = 3;
+            this.Value2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 111);
+            this.Controls.Add(this.Value2);
+            this.Controls.Add(this.Accept);
+            this.Controls.Add(this.Value1);
+            this.Controls.Add(this.CurList2);
+            this.Controls.Add(this.CurList1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CurList1;
+        private System.Windows.Forms.ComboBox CurList2;
+        private System.Windows.Forms.TextBox Value1;
+        private System.Windows.Forms.Button Accept;
+        private System.Windows.Forms.Label Value2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, Tuple<string, double>> currancy = new Dictionary<int, Tuple<string, double>>(); //Глобальный словарь, тут будут находиться (номер, кортеж(валюта, курс))
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] mass = new string[1400]; // Массив строк, в него помещается построчно ХТМЛ файл сайта
+            string line;
+            int k = 0;
+            int x, y, x1, y1 = 0;
+            int i = 0;
+            
+
+            WebClient client = new WebClient(); //созадем класс, короче, вся мура, чтобы вытащить строки ХТМЛ в массик
+            client.Encoding = Encoding.UTF8;
+            Stream data = client.OpenRead("https://cbr.ru/currency_base/daily/");
+            StreamReader reader = new StreamReader(data, Encoding.UTF8);
+            while ((line = reader.ReadLine()) != null)
+            {
+                mass[i] = line;
+                i++;
+            }
+            data.Close();
+            reader.Close();
+            
+
+            for (int j = 292; j < mass.Length; j++) // Парсим короче (с 292 строчки, потому что раньше все равно смысла нет)
+            {
+                
+                if (mass[j].IndexOf("<tr") != -1)// находим строки, где сидят данные по валютам и курсам (там надо смотреть код дока, если есть желание разобраться
+                {
+
+                    x = mass[j + 4].IndexOf('>') + 1;
+                    y = mass[j + 4].LastIndexOf('<') - mass[j + 4].IndexOf('>') - 1;
+
+                    
+                    x1 = mass[j + 5].IndexOf('>') + 1;
+                    y1 = mass[j + 5].LastIndexOf('<') - mass[j + 5].IndexOf('>') - 1;
+                    CurList1.Items.Add(mass[j + 4].Substring(x, y));
+                    CurList2.Items.Add(mass[j + 4].Substring(x, y));
+                    currancy.Add(k, new Tuple<string, double>(mass[j + 4].Substring(x, y), Convert.ToDouble(mass[j + 5].Substring(x1, y1)))); //Заполняем словарик кстате
+
+                    k += 1;
+                }
+            }
+
+            CurList1.Text = currancy[0].Item1; // это чето все равно не работает, забейте
+            CurList2.Text = currancy[0].Item1;
+
+
+
+
+        }
+
+        private void Accept_Click(object sender, EventArgs e)
+        {
+            double x = -1, y = -1; // Тута короче все подсчеты курсов и вывод результата. Покопаться - разберешься
+            for (int i = 0; i < currancy.Count; i++)
+            {
+                if (CurList1.Text == currancy[i].Item1) x = currancy[i].Item2;
+            }
+            for (int i = 0; i < currancy.Count; i++)
+            {
+                if (CurList2.Text == currancy[i].Item1) y = currancy[i].Item2;
+            }
+            Value2.Text = (Convert.ToDouble(Value1.Text) * y / x).ToString();
+        }
+    }
+}
